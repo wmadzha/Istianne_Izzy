7718448f15a6e1e4d56e8f825a5403d3e2f8b3f0
Wan Mohd Adzha <wmadzha11@hotmail.com>
2018-10-25 12:55:19 +0800

    Create Helper.cs

---
+++ EngineUtilities/Helper.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.Caching;
+
+namespace Grodi_Cache_Engine.EngineUtilities
+{
+    public static class Helper
+    {
+        public static bool CastCache<T>(string CacheName , List<T> Data)
+        {
+            try
+            {
+                ObjectCache cache = MemoryCache.Default;
+                try
+                {
+                    cache.Remove(CacheName);
+                }
+                catch (Exception)
+                {
+                }
+                CacheItemPolicy policy = new CacheItemPolicy { AbsoluteExpiration = DateTimeOffset.Now.AddHours(1000) };
+                cache.Add(CacheName, Data, policy);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
