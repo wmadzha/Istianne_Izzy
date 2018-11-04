e38321ae76c8e6b0be68b9b918b32684055320f8
Wan Mohd Adzha <wmadzha11@hotmail.com>
2018-11-04 23:07:29 +0800

    Create CacheName.cs

---
+++ Constants/CacheName.cs
@@ -0,0 +1,20 @@
+
+namespace Istianne_Izzy.Constants
+{
+    public static class CacheName
+    {
+        public static string BookingSlot
+        {
+           get{ return "BookingSlots"; }
+        }
+        public static string BookingSlotClient
+        {
+            get{ return "BookingSlotClients"; }        
+        }
+        public static string CompletedOrderToken
+        {
+            get{ return "CompletedOrderTokens"; } 
+        }
+       
+    }
+}
